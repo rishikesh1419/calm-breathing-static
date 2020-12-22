569ce0f7f21e7c653042672f94f250f51ea6da3e
Rishikesh Kadam <rishikesh1419@gmail.com>
2020-12-22 19:20:14 +0530

    Basic layout

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Ok");
+var btnBox = document.getElementById("btnBox");
+var btn478 = document.getElementById("btn478");
+var btn711 = document.getElementById("btn711");
+
+var buttons = [btnBox, btn478, btn711];
+
+function clicked (index) {
+    for (var i=0; i < buttons.length; i++) {
+        if (i === index) {
+            buttons[i].classList.add('btn-primary');
+            buttons[i].classList.remove('btn-light');
+        }
+        else {
+            buttons[i].classList.add('btn-light');
+            buttons[i].classList.remove('btn-primary');
+        }
+    }
+};
+
+btnBox.addEventListener("click", clicked.bind(null, 0));
+btn478.addEventListener("click", clicked.bind(null, 1));
+btn711.addEventListener("click", clicked.bind(null, 2));
