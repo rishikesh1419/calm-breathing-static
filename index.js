589acab6d388bb2019a9fd7e6f153da8904cd5fc
Rishikesh Kadam <rishikesh1419@gmail.com>
2020-12-26 00:40:35 +0530

    Bug fixes

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
 const btnBox = document.getElementById("btnBox");
 const btn478 = document.getElementById("btn478");
 const btn711 = document.getElementById("btn711");
+let gradient = document.getElementById("gradient");
+let breather = document.getElementById("breather");
+let pointer = document.getElementById("pointer");
 
-const buttons = [btnBox, btn478, btn711];
+const buttons = [
+    {
+        element: btnBox,
+        gradientClasses: "gradient gradient-box",
+        breatherClasses: "breather scaleBox",
+        pointerClasses: "pointer-container pointer-box",
+    },
+    {
+        element: btn478,
+        gradientClasses: "gradient gradient-478",
+        breatherClasses: "breather scale478",
+        pointerClasses: "pointer-container pointer-478",
+    },
+    {
+        element: btn711,
+        gradientClasses: "gradient gradient-711",
+        breatherClasses: "breather scale711",
+        pointerClasses: "pointer-container pointer-711",
+    },
+];
 
 const clicked = index => {
-    for (var i=0; i < buttons.length; i++) {
+    buttons.map((btn, i) => {
         if (i === index) {
-            buttons[i].classList.add('btn-primary');
-            buttons[i].classList.remove('btn-light');
+            btn.element.classList.add('btn-primary');
+            btn.element.classList.remove('btn-light');
+            breather.className = btn.breatherClasses;
+            breather.removeChild(pointer);
+            breather.innerHTML = `<div id="pointer" class="${btn.pointerClasses}"><span class="pointer"></span></div>` + breather.innerHTML;
+            breather = document.getElementById("breather");
+            pointer = document.getElementById("pointer");
+            gradient = document.getElementById("gradient");
+            gradient.className = btn.gradientClasses;
         }
         else {
-            buttons[i].classList.add('btn-light');
-            buttons[i].classList.remove('btn-primary');
+            btn.element.classList.add('btn-light');
+            btn.element.classList.remove('btn-primary');
         }
-    }
+    });
 };
 
 btnBox.addEventListener("click", clicked.bind(null, 0));
